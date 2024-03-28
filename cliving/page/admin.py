fe5e7ddf3d103274c16ba4b29f74819204703274
rktlskan021 <rktlskan021@gmail.com>
2024-03-28 21:32:46 +0900

    feat: ERD 설계에 따른 모델 생성
    page APP 생성
    각 모델 생성
    .gitignore 위치 최상위 디렉토리로 이동

---
+++ cliving/page/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Page)
+admin.site.register(Clip)
+admin.site.register(Hold)
+admin.site.register(Frame)
