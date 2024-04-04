7613c05e9f4838c238a1170b9affb2c17650c9b3
forestsol <forestsol1214@gmail.com>
2024-04-04 19:31:15 +0900

    feat: v1으로 url 변경

--- cliving/page/urls.py
+++ cliving/page/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
-from .views import PageViewSet #, ClipViewSet, FrameViewSet, HoldViewSet
+from .views import PageViewSet, ClipViewSet, FrameViewSet, HoldViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('page', PageViewSet)
-# router.register('clip', ClipViewSet)
-# router.register('Frame', FrameViewSet)
-# router.register('hold', HoldViewSet)
+router.register('clip', ClipViewSet)
+router.register('Frame', FrameViewSet)
+router.register('hold', HoldViewSet)
 
 urlpatterns = [
-    path('', include([
+    path('v1/', include([
         path('', include(router.urls)),
     ])),
 ]
