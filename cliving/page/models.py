ec1299e6e741de974442e2c3c9e732739d1dd67c
forestsol <forestsol1214@gmail.com>
2024-05-19 10:45:07 +0900

    Update models.py

--- cliving/page/models.py
+++ cliving/page/models.py
@@ -22,11 +22,12 @@ class Page(models.Model):
     bouldering_clear_color = ArrayField(models.IntegerField(choices=COLOR_CHOICES), verbose_name="bcc")
     endurance_clear_color = ArrayField(models.IntegerField(choices=COLOR_CHOICES), verbose_name="ecc")
     climbing_center_name = models.CharField(max_length=20, verbose_name="name")
-    start_time = models.TimeField(blank=True, null=True, verbose_name="start")
-    end_time = models.TimeField(blank=True, null=True, verbose_name="end")
+    start_time = models.TimeField(blank=True, null=True, verbose_name="start")  #암장에서 첫번째 영상을 시작한 시간
+    end_time = models.TimeField(blank=True, null=True, verbose_name="end")  #암장에서 마지막 영상을 끝낸 시간(암장에서 있던 시간을 기록)
+    game_play_time = models.TimeField(blank=True, null=True, verbose_name="game")   #게임 플레이 시간(영상들 플레이타임을 더함)
 
 class Video(models.Model):
-    # page_id = models.ForeignKey(Page, related_name="page", on_delete=models.CASCADE)
+    page_id = models.ForeignKey(Page, related_name="page", on_delete=models.CASCADE)
     videofile = models.FileField(upload_to='videofiles/')
     date_created = models.DateTimeField(auto_now_add=True)  #레코드 처음 생성될 때 자동으로 현재 시간 저장.
 
