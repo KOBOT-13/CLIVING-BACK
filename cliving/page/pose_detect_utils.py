25a8cea33ff82f1bd1e9b13dc7ec4bf115e20a8c
colabribble <kmw5691@naver.com>
2024-06-26 01:51:25 +0900

    해상도 비율 설정

--- cliving/page/pose_detect_utils.py
+++ cliving/page/pose_detect_utils.py
@@ -31,15 +31,15 @@ def detect_pose(video):
         x1, x2, y1, y2 = 0.4, 0.6, 0.2, 0.4
         print("'custom_error': No top hold found for the latest first image. However, we will proceed with the default values x1, x2, y1, y2 = 0.4, 0.6, 0.2, 0.4.")
     else:
-        y1, y2, x1, x2 = (top_hold.x1/ 2087, top_hold.x2/ 2087, top_hold.y1/ 1179, top_hold.y2/ 1179)
+        y1, y2, x1, x2 = (top_hold.x1/ 3024, top_hold.x2/ 3024, top_hold.y1/ 4032, top_hold.y2/ 4032)
         print("Top Hold : ", x1,y1,x2,y2)
 
     if not bottom_hold:
         y3, y4, x3, x4 = 0.1, 0.2, 0.1, 0.2
         print("'custom_error': No bottom hold found for the latest first image. However, we will proceed with the default values x3, x4, y3, y4 = 0.1, 0.2, 0.1, 0.2.")
     else:
-        y3, y4, x3, x4 = (bottom_hold.x1/ 2087, bottom_hold.x2/ 2087, bottom_hold.y1/ 1179, bottom_hold.y2/ 1179)
-        print("Bottom Hold : ", x3,y3,x4,y4)
+        y3, y4, x3, x4 = (bottom_hold.x1/ 3024, bottom_hold.x2/ 3024, bottom_hold.y1/ 4032, bottom_hold.y2/ 4032)
+        print("Bottom Hold : ", y3, y4, x3, x4)
         y_fail_point1= y3 * 1.1
         y_fail_point2= y4 * 1.1
 
