56ada81d508cef303dc3e7a5e521bfefb4eae328
rktlskan021 <rktlskan021@gmail.com>
2024-06-26 20:37:21 +0900

    fix: YOLO Model 좌표값 좌우반전 현상 버그 수정

--- cliving/page/pose_detect_utils.py
+++ cliving/page/pose_detect_utils.py
@@ -30,21 +30,22 @@ def detect_pose(video):
         x1, x2, y1, y2 = 0.4, 0.6, 0.2, 0.4
         print("'custom_error': No top hold found for the latest first image. However, we will proceed with the default values x1, x2, y1, y2 = 0.4, 0.6, 0.2, 0.4.")
     else:
-        y1, y2, x1, x2 = (top_hold.x1, top_hold.x2, top_hold.y1, top_hold.y2)
+        x1, x2, y1, y2 = (top_hold.x1, top_hold.x2, top_hold.y1, top_hold.y2)
         print("Top Hold : ", x1,y1,x2,y2)
-        y1, y2, x1, x2 = (top_hold.x1/ 3024, top_hold.x2/ 3024, top_hold.y1/ 4032, top_hold.y2/ 4032)
+        x1, x2, y1, y2 = (top_hold.x1/ 3024, top_hold.x2/ 3024, top_hold.y1/ 4032, top_hold.y2/ 4032)
         print("Top Hold : ", x1,y1,x2,y2)
 
     if not bottom_hold:
         y3, y4, x3, x4 = 0.1, 0.2, 0.1, 0.2
         print("'custom_error': No bottom hold found for the latest first image. However, we will proceed with the default values x3, x4, y3, y4 = 0.1, 0.2, 0.1, 0.2.")
     else:
-        y3, y4, x3, x4 = (bottom_hold.x1, bottom_hold.x2, bottom_hold.y1, bottom_hold.y2)
+        x3, x4, y3, y4 = (bottom_hold.x1, bottom_hold.x2, bottom_hold.y1, bottom_hold.y2)
         print("Bottom Hold : ", x3,y3,x4,y4)
-        y3, y4, x3, x4 = (bottom_hold.x1/ 3024, bottom_hold.x2/ 3024, bottom_hold.y1/ 4032, bottom_hold.y2/ 4032)
+        x3, x4, y3, y4 = (bottom_hold.x1/ 3024, bottom_hold.x2/ 3024, bottom_hold.y1/ 4032, bottom_hold.y2/ 4032)
         print("Bottom Hold : ", x3,y3,x4,y4)
         y_fail_point2= y4 * 1.05
-
+        if y_fail_point2 > 1 :
+            y_fail_point2 = y4
 
 
     cap = cv2.VideoCapture(video.videofile.path)
@@ -103,12 +104,13 @@ def detect_pose(video):
                 skip_frames = 60  # 60프레임 건너뛰기
                 continue
             # 성공 탐지~ 
+
             if is_started and not is_success and (
-                (left_wrist is not None and x1 <= left_wrist.x <= x2 and y1 <= left_wrist.y <= y2) or 
-                (right_wrist is not None and x1 <= right_wrist.x <= x2 and y1 <= right_wrist.y <= y2)
+                (left_wrist is not None and x1 <= 1-left_wrist.x <= x2 and y1 <= left_wrist.y <= y2) or 
+                (right_wrist is not None and x1 <= 1-right_wrist.x <= x2 and y1 <= right_wrist.y <= y2)
             ):
                 is_success = True
-                skip_frames = 30 
+                skip_frames = 30
 
     # is_started가 True일 때만 실패/성공 체크
             if is_started:
