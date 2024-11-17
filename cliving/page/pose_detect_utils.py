1b0d5188bdc0d827b10168c745c48b88b5ce850d
OnlyWonHand <113083948+IamWonILuvWon@users.noreply.github.com>
2024-11-17 16:31:08 +0900

    FIX: 프레임 수정

--- cliving/page/pose_detect_utils.py
+++ cliving/page/pose_detect_utils.py
@@ -32,13 +32,13 @@ def detect_pose(video):
         x1, x2, y1, y2 = 0.4, 0.6, 0.2, 0.4
         print("'custom_error': No top hold found for the latest first image. However, we will proceed with the default values x1, x2, y1, y2 = 0.4, 0.6, 0.2, 0.4.")
     else:
-        x1, x2, y1, y2 = (top_hold.x1/ 3024, top_hold.x2/ 3024, top_hold.y1/ 4032, top_hold.y2/ 4032)
+        x1, x2, y1, y2 = (top_hold.x1/ 1080, top_hold.x2/ 1080, top_hold.y1/ 1920, top_hold.y2/ 1920)
 
     if not bottom_hold:
         y3, y4, x3, x4 = 0.1, 0.2, 0.1, 0.2
         print("'custom_error': No bottom hold found for the latest first image. However, we will proceed with the default values x3, x4, y3, y4 = 0.1, 0.2, 0.1, 0.2.")
     else:
-        x3, x4, y3, y4 = (bottom_hold.x1/ 3024, bottom_hold.x2/ 3024, bottom_hold.y1/ 4032, bottom_hold.y2/ 4032)
+        x3, x4, y3, y4 = (bottom_hold.x1/ 1080, bottom_hold.x2/ 1080, bottom_hold.y1/ 1920, bottom_hold.y2/ 1920)
         y_fail_point2= y4 * 1.05
         if y_fail_point2 > 1 :
             y_fail_point2 = y4
